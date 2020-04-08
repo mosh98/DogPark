6bb7be70f4a45fce7c39a6830ee87a89034b8f2b
moslehmahamud <mosleh.edu@gmail.com>
2020-04-08 21:28:01 +0200

    Initial commit

---
+++ src/main/java/com/example/HundRastGard/HundRastGardApplication.java
@@ -0,0 +1,13 @@
+package com.example.HundRastGard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HundRastGardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HundRastGardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HundRastGard/HundRastGardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HundRastGard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HundRastGardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
